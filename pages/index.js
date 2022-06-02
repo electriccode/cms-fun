1dad5d6276c0a9a410a879c9361c9014edc6f81d
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2022-06-02 14:30:29 +0530

    passing dummy data from getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
 
-export default function IndexPage() {
+export default function IndexPage(props) {
+  // props argument will receive data returned from getServerSideProps
   return (
     <div>
-      Hello World!!!{" "}
+      {/* // print the hello object passed from getServerSideProps */}
+      {props.hello}
       <Link href="/about">
         <a>About</a>
       </Link>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: { hello: "World" } // will be passed to the page component as props
+  };
 }
