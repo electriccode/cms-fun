d5c700f44c235c0925ee168afecfa25e643f252b
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2022-06-02 14:43:17 +0530

    fetch contentful data

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,36 @@ export default function IndexPage(props) {
 }
 
 export async function getServerSideProps(context) {
+  const graphqlQuery = `query {
+    pageCollection {
+      items {
+        title
+      }
+    }
+  }`.replace(/\n/g, " "); // Create a one liner query replacing all new lines with space
+
+  // Make the API call
+  const response = await fetch(
+    "https://graphql.contentful.com/content/v1/spaces/" + process.env.SPACE_ID,
+    {
+      headers: {
+        authorization: "Bearer " + process.env.TOKEN,
+        "content-type": "application/json"
+      },
+      body: '{"query":"' + graphqlQuery + '","variables":null}',
+      method: "POST"
+    }
+  );
+
+  // Convert the Response object into JSON
+  const data = await response.json();
+
+  // Convert the JSON to string
+  const dataString = JSON.stringify(data);
   return {
-    props: { hello: "World" } // will be passed to the page component as props
+    props: {
+      hello: dataString,
+      data
+    } // will be passed to the page component as props
   };
 }
